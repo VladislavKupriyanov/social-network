db21d572d981531a1a09b03a6aa61fe5062101da
VladislavKupriyanov <kupriyanov6308@gmail.com>
2021-04-19 22:22:07 +0300

    updated

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -4,19 +4,18 @@ import { ActionTypes, addPostAC, PostType, updateNewPostTextAC } from '../../../
 import { Post } from './Post/Post';
 
 type PropsType = {
-    posts: Array<PostType>
-    newPostText: string
-    dispatch: (action: ActionTypes) => void
+    posts: Array<PostType>;
+    newPostText: string;
+    dispatch: (action: ActionTypes) => void;
 };
 
 export const MyPosts: React.FC<PropsType> = ({ posts, newPostText, dispatch }) => {
-
-    const postsElements = posts.map(p => <Post key={p.id} post={p.post} likeCount={p.likeCount} />);
+    const postsElements = posts.map((p) => <Post key={p.id} post={p.post} likeCount={p.likeCount} />);
 
     const onClickAddPost = () => {
         if (newPostText !== '') {
             dispatch(addPostAC());
-        };
+        }
     };
 
     const onChangeNewPostText = (e: ChangeEvent<HTMLTextAreaElement>) => {
@@ -26,11 +25,15 @@ export const MyPosts: React.FC<PropsType> = ({ posts, newPostText, dispatch }) =
     return (
         <div className={s.my_posts}>
             <h4>Мои посты</h4>
-            <textarea value={newPostText}
+            <textarea
+                value={newPostText}
                 onChange={onChangeNewPostText}
                 className={s.add_post_area}
-                placeholder='Что у вас нового?' />
-            <button onClick={onClickAddPost} className={s.send_btn}>Добавить пост</button>
+                placeholder="Что у вас нового?"
+            />
+            <button onClick={onClickAddPost} className={s.send_btn}>
+                Добавить пост
+            </button>
             {postsElements}
         </div>
     );
